f5386f283676898ac0ba1c3b120261000b6241ee
cursedastronaut <g.martineaux@student.isartdigital.com>
2023-11-30 15:34:04 +0100

    Ajout de dossier de test, et de ressources.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
 #include "menu_files.hpp"
 
 int main(int argc, char* argv[]) {
-	rgbVec test;
-	test.red = {{0,0,0,0}, {0,0,0,0}, {0,0,0,255}, {0,0,255,255}};
-	test.blue = {{255,255,0,0}, {255,0,0,0}, {0,0,0,0}, {0,0,0,0}};
-	test.green = {{0,0,255,255}, {0,255,0,0}, {255,0,0,0}, {0,0,0,0}};
-	//Image image = Image(test.red, test.blue, test.green);
-	Image image = Image("Couleur.ppm");
-	image.afficher();
-	image.ecrire("Couleur_GENERATED.ppm");
+	Image image = Image("Flowerbed-large.ppm");
+	image.niveauxGris().ecrire("Couleur_GENERATED.ppm");
 	return 0;
 }
